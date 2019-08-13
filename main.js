9a69f7a3f06284178e1fa0f76f779074f0cde54d
Devika <devikamehr@gmail.com>
2019-08-13 08:57:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+$("#address-search").on( "click", function() {
+  var encodedaddress = encodeURIComponent($("#address").val());
+  $.getJSON( "https://www.googleapis.com/civicinfo/v2/voterinfo?address="+encodedaddress+"INSERTKEYHERE",
+  function( json ) {
+    var name  = json.pollingLocations[0].address.locationName;
+    var street  = json.pollingLocations[0].address.line1;
+    var city  = json.pollingLocations[0].address.city;
+    var state  = json.pollingLocations[0].address.state;
+    var zip  = json.pollingLocations[0].address.zip;
+    var hours  = json.pollingLocations[0].pollingHours;
+    $("#search-result").text(name + " " + street + " " +  city + " " + state + " " +  zip + " " +  hours);
+
+ });
+});
+});
